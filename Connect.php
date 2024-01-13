df5bcb29348f68b190966aa94ce3b5c621a04815
charafeddinejaafari <121683284+charafeddinejaafari@users.noreply.github.com>
2024-01-13 13:58:05 +0100

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost"; // Remplacez par votre nom de serveur
 $username = "root"; // Remplacez par votre nom d'utilisateur de la base de données
-$password = ""; // Remplacez par votre mot de passe de la base de données
+$password = "charaf"; // Remplacez par votre mot de passe de la base de données
 $dbname = "basedata"; // Remplacez par le nom de votre base de données
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
